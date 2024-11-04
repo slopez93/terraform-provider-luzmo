37b4120f6503ea1c9b20c1b41b03d4532f14b7fd
Santi Lopez <santiago.lopez@lansweeper.com>
2024-11-04 10:12:19 +0100

    replacement

---
+++ internal/provider/provider_test.go
@@ -0,0 +1,30 @@
+package provider
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+)
+
+const (
+	// providerConfig is a shared configuration to combine with the actual
+	// test configuration so the HashiCups client is properly configured.
+	// It is also possible to use the HASHICUPS_ environment variables instead,
+	// such as updating the Makefile and running the testing through that tool.
+	providerConfig = `
+		provider "luzmo" {
+			api_key     = "test_api_key"
+			api_token   = "test_api_token"
+			api_version = "0.1.0"
+		}
+	`
+)
+
+var (
+	// testAccProtoV6ProviderFactories are used to instantiate a provider during
+	// acceptance testing. The factory function will be invoked for every Terraform
+	// CLI command executed to create a provider server to which the CLI can
+	// reattach.
+	testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
+		"luzmo": providerserver.NewProtocol6WithError(New("test")()),
+	}
+)
